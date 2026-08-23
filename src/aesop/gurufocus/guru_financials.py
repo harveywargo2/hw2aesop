143ada1b1a71dfdb043bb144079573d69405d8fa
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2026-08-23 09:57:49 -0400

    Update guru_financials.py

--- src/aesop/gurufocus/guru_financials.py
+++ src/aesop/gurufocus/guru_financials.py
@@ -10,17 +10,23 @@ class GuruFinancialHistory:
     ticker: str
     ddr_dict: dict = field(init=False, repr=False, default=None)
     ddr_fin_parameters_dict: dict = field(init=False, repr=False, default=None)
+    ddr_fin_annuals_dict: dict = field(init=False, repr=False, default=None)
+    ddr_fin_quarterly_dict: dict = field(init=False, repr=False, default=None)
+    ddr_fin_annuals_df: object = field(init=False, repr=False, default=None)
+    ddr_fin_quarterly_df: object = field(init=False, repr=False, default=None)
 
 
     def __post_init__(self):
         self.ddr_dict = self._guru_api()
         self.ddr_fin_parameters_dict = self._fin_parameters()
-        self.ddr_fin_annual_dict = self._fin_annuals()
-        self.ddr_fin_quarter_dict = self._fin_quarterly()
-
+        self.ddr_fin_annuals_dict = self._fin_annuals()
+        self.ddr_fin_quarterly_dict = self._fin_quarterly()
+        self.ddr_fin_annuals_df = self._fin_annuals_df()
+        self.ddr_fin_quarterly_df = self._fin_quarterly_df()
 
 
     def _guru_api(self):
+
         url = f'https://api.gurufocus.com/public/user/{self.token}/stock/{self.ticker}/financials'
 
         try:
@@ -57,5 +63,30 @@ class GuruFinancialHistory:
 
     def _fin_quarterly(self):
         data = self.ddr_dict['financials']['quarterly']
+
         return data
+
+
+    def _fin_annuals_df(self):
+        data = self.ddr_fin_annuals_dict
+        df_0 = pd.DataFrame()
+        df_1 = pd.json_normalize(data)
+
+        for item, value in df_1.items():
+            series_explode = pd.Series(value, name=item).explode(ignore_index=True)
+            df_0 = pd.concat([df_0, series_explode.to_frame()], axis=1)
+
+        return df_0
+
+
+    def _fin_quarterly_df(self):
+        data = self.ddr_fin_quarterly_dict
+        df_0 = pd.DataFrame()
+        df_1 = pd.json_normalize(data)
+
+        for item, value in df_1.items():
+            series_explode = pd.Series(value, name=item).explode(ignore_index=True)
+            df_0 = pd.concat([df_0, series_explode.to_frame()], axis=1)
+
+        return df_0
 
