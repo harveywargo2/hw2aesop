1d3de987e7a16e5fabe3ea14d406e14bfefff953
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2026-08-24 08:41:44 -0400

    Create common.py

---
+++ src/aesop/common.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+
+mod_path = Path(__file__).parent
+
+
+def find_desktop_path():
+    home = os.path.expanduser('~')
+
+    if os.name == 'nt':
+        desktop_path = os.path.join(home, 'Desktop')
+    elif os.name == 'posix':
+        desktop_path = os.path.join(home, 'Desktop')
+    else:
+        return None
+
+    return desktop_path
+
