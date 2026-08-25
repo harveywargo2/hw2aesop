75e6606f325433a3ecb43795daf3c6f4ec80092d
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2026-08-25 07:58:29 -0400

    Update __init__.py

--- src/aesop/__init__.py
+++ src/aesop/__init__.py
@@ -1,7 +1,7 @@
+from aesop.gurufocus.guru_api import *
 from aesop.gurufocus.guru_dividend import *
 from aesop.gurufocus.guru_financials import *
 from aesop.gurufocus.guru_price import *
-from aesop.gurufocus.guru_stock import *
 from aesop.gurufocus.guru_utils import *
 
 
